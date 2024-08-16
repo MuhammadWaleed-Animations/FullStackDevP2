70cee1bde3a2610743de794b3e5d14d2d6e8308c
ShahzebMubashar <shahzebmubashar7@gmail.com>
2024-08-16 20:52:24 +0500

    Conflict resolve

---
+++ src/components/AutomationTitle.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Typography, Row, Col, Button } from 'antd';
+
+const { Title, Text } = Typography;
+
+const HeaderSection = () => {
+  return (
+    <div style={{ background: '#003399', padding: '50px 0', textAlign: 'center', color: '#fff' }}>
+      <Row justify="center">
+        <Col>
+        
+          <Title level={1} style={{ color: '#fff', marginTop: '10px' }}>Automation & Integration</Title>
+          <Text style={{ display: 'block', marginBottom: '20px', maxWidth: '600px', margin: '20px auto', color: '#fff',fontSize: '22px' }}>
+          Analyzinn Solutions drives automation and integration to reduce manual efforts.
+          </Text>
+          <Button type="primary" size="large" style={{ backgroundColor: '#000', borderColor: '#003399', marginTop: '0px' }}>
+            Free Consultation Today
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default HeaderSection;
